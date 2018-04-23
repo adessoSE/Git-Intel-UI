98d0e786ac14d2267a73b9b651d7d889a7d2ad74
Dario Braun <dario.braun@adesso.de>
2018-04-23 16:32:24 +0200

    Create team page, create members page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent }   from './dashboard/dashboard.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { MembersComponent } from './members/members.component';
 import { TeamsComponent } from './teams/teams.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
@@ -13,11 +13,11 @@ const routes: Routes = [
   { path: 'members', component: MembersComponent },
   { path: 'teams', component: TeamsComponent },
   { path: 'repositories', component: RepositoriesComponent },
-  { path: 'external-repositories', component: ExternalRepositoriesComponent}
+  { path: 'external-repositories', component: ExternalRepositoriesComponent }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
