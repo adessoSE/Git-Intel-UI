7d7c01095572f36d7bd76d2af012c8ebb1d655f3
Dario Braun <dario.braun@adesso.de>
2018-07-27 12:52:24 +0200

    Fix attribute names for member entitiy

--- src/app/entities/member.ts
+++ src/app/entities/member.ts
@@ -7,9 +7,9 @@ export class Member {
     public avatarURL: string;
     public githubURL: string;
     public organization: Organization;
-    public commits: number;
-    public pullRequests: number;
-    public issues: number;
+    public amountPreviousCommits: number;
+    public amountPreviousPullRequests: number;
+    public amountPreviousIssues: number;
     public previousCommits: ChartJsData;
     public previousPullRequests: ChartJsData;
     public previousIssues: ChartJsData;
