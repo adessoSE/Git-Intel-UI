be3fe8a332c8893ac2b24bc5cef523b36808fb93
Dario Braun <dario.braun@adesso.de>
2018-08-31 11:32:01 +0200

    Change REST requests from POST to GET

--- src/app/services/data-pull.service.ts
+++ src/app/services/data-pull.service.ts
@@ -16,41 +16,36 @@ export class DataPullService {
   constructor(private _http: HttpClient) { }
 
   /**
-   * Set the organization's name as needed http parameter for the post operation.
-   * Post to the correct REST endpoint and receive appropriate data. 
+   * 
+   * GET Request to the correct REST endpoint and receive appropriate data. 
    * @param organizationName 
    */
   requestOrganization(organizationName: string): Observable<Organization> {
-    this.params = new HttpParams().set('name', organizationName);
-    let requestURL = this.gitStalkerURL + 'organizationdetail';
+    let requestURL = this.gitStalkerURL + 'organizationdetail' + '/' + organizationName;
     console.log("ORGANIZATION DATA REQUESTED");
     return this._http.post<Organization>(requestURL, this.params);
   }
 
   requestMembers(organizationName: string): Observable<Member[]> {
-    this.params = new HttpParams().set('name', organizationName);
-    let requestURL = this.gitStalkerURL + 'members';
+    let requestURL = this.gitStalkerURL + 'members' + '/' + organizationName;
     console.log("MEMBER DATA REQUESTED");
     return this._http.post<Member[]>(requestURL, this.params);
   }
 
   requestRepositories(organizationName: string): Observable<Repository[]> {
-    this.params = new HttpParams().set('name', organizationName);
-    let requestURL = this.gitStalkerURL + 'repositories';
+    let requestURL = this.gitStalkerURL + 'repositories' + '/' + organizationName;
     console.log("REPOSITORY DATA REQUESTED");
     return this._http.post<Repository[]>(requestURL, this.params);
   }
 
   requestTeams(organizationName: string): Observable<Team[]> {
-    this.params = new HttpParams().set('name', organizationName);
-    let requestURL = this.gitStalkerURL + 'teams';
+    let requestURL = this.gitStalkerURL + 'teams' + '/' + organizationName;
     console.log("TEAM DATA REQUESTED");
     return this._http.post<Team[]>(requestURL, this.params);
   }
 
   requestExternalRepositories(organizationName: string): Observable<Repository[]> {
-    this.params = new HttpParams().set('name', organizationName);
-    let requestURL = this.gitStalkerURL + 'externalrepositories';
+    let requestURL = this.gitStalkerURL + 'externalrepositories' + '/' + organizationName;
     console.log("EXTERNAL REPOSITORY DATA REQUESTED");
     return this._http.post<Repository[]>(requestURL, this.params);
   }
