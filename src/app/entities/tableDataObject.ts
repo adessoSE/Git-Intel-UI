d9dc993678670f4baabe0ecd19ac6b2113665be8
Dario Braun <dario.braun@adesso.de>
2018-09-27 13:16:52 +0200

    Add Issues/Commits/PRs tables to member page

---
+++ src/app/entities/tableDataObject.ts
@@ -0,0 +1,9 @@
+export class TableDataObject {
+    public date: String;
+    public value: String;
+
+    constructor(key, value) {
+        this.date = key;
+        this.value = value;
+    }
+  };
--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { ChartJsData } from '../entities/chartJS';
 import { Member } from '../entities/member';
 import { DataPullService } from '../services/data-pull.service';
 import { GlobalNavigationService } from '../services/global-navigation.service';
+import { TableDataObject } from '../entities/tableDataObject';
 
 @Component({
   selector: 'app-member',
@@ -16,6 +17,10 @@ export class MemberComponent implements OnInit {
   chartCommits: ChartJsData;
   chartPRs: ChartJsData;
   chartIssues: ChartJsData;
+
+  commitsTableData: TableDataObject[] = [];
+  pullRequestsTableData: TableDataObject[] = [];
+  issuesTableData: TableDataObject[] = [];
 
   constructor(
     private dataPullService: DataPullService,
@@ -45,6 +50,7 @@ export class MemberComponent implements OnInit {
         }
       }
       this.initGraphs();
+      this.processTableData();
     });
   }
 
@@ -64,5 +70,34 @@ export class MemberComponent implements OnInit {
       data: [{ data: this.member.previousIssues.chartJSDataset, label: "Issues" }],
       caption: "Latest Issues"
     };
+  }
+
+  /**
+   * Processes commit/issue/pr data for Table by creating appropriate TableDataObjects and pushing them into an array for iteration.
+   */
+  processTableData() {
+    let tableDataObject: TableDataObject;
+    for (var key in this.member.previousCommitsWithLink) {
+      if (this.member.previousCommitsWithLink.hasOwnProperty(key)) {
+        // Format date string so it is a valid argument for the Date() constructor
+        let year = key.split(" ");
+        console.log(year);
+        tableDataObject = new TableDataObject(new Date(key), this.member.previousCommitsWithLink[key]);
+        this.commitsTableData.push(tableDataObject);
+      }
+    }
+    for (var key in this.member.previousPullRequestsWithLink) {
+      if (this.member.previousPullRequestsWithLink.hasOwnProperty(key)) {
+        tableDataObject = new TableDataObject(key, this.member.previousPullRequestsWithLink[key]);
+        this.pullRequestsTableData.push(tableDataObject);
+      }
+    }
+    for (var key in this.member.previousIssuesWithLink) {
+      if (this.member.previousIssuesWithLink.hasOwnProperty(key)) {
+        console.log(new Date(key));
+        tableDataObject = new TableDataObject(key, this.member.previousIssuesWithLink[key]);
+        this.issuesTableData.push(tableDataObject);
+      }
+    }
   }
 }
